137c9c6c33bc2c7cb536240ccb06d7e5eedd2f39
Ivonne Garcia <garcia.murcio.ivonne@gmail.com>
2026-07-08 16:09:49 -0600

    pedido updated

--- src/main/java/com/org/mixtecatl/main/controllers/PedidoController.java
+++ src/main/java/com/org/mixtecatl/main/controllers/PedidoController.java
@@ -1,4 +1,11 @@
 package com.org.mixtecatl.main.controllers;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pedidos")
 public class PedidoController {
+
 }
--- src/main/java/com/org/mixtecatl/main/models/Pedido.java
+++ src/main/java/com/org/mixtecatl/main/models/Pedido.java
@@ -1,4 +1,52 @@
 package com.org.mixtecatl.main.models;
 
+import java.util.Date;
+import java.util.List;
+
 public class Pedido {
+  private Long idCliente;
+  private Long idPedido;
+  private List<Long> platillos;
+  private Date fecha;
+  private Boolean EstadoPedido;
+
+    public Pedido(Long idCliente, Long idPedido, List<Long> platillos, Date fecha, Boolean estadoPedido) {
+        this.idCliente = idCliente;
+        this.idPedido = idPedido;
+        this.platillos = platillos;
+        this.fecha = fecha;
+        EstadoPedido = estadoPedido;
+    }
+
+    //getter
+    public Long getIdCliente() {return idCliente;}
+    public Long getIdPedido() {return idPedido;}
+    public List<Long> getPlatillos() {return platillos;}
+    public Date getFecha() {return fecha;}
+    public Boolean getEstadoPedido() {return EstadoPedido;}
+
+    //Setter
+
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public void setIdPedido(Long idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public void setPlatillos(List<Long> platillos) {
+        this.platillos = platillos;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setEstadoPedido(Boolean estadoPedido) {
+        EstadoPedido = estadoPedido;
+    }
+
+
 }
