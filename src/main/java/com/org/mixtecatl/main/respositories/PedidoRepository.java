1ad8ef3b0e1d81206ba2ff3ba7852d5f8b41d0ac
MichaelC <michaelcastrorom30@gmail.com>
2026-07-10 13:46:10 -0600

    Add implementation module [pedido]

--- src/main/java/com/org/mixtecatl/main/respositories/PedidoRepository.java
+++ src/main/java/com/org/mixtecatl/main/respositories/PedidoRepository.java
@@ -1,4 +1,10 @@
 package com.org.mixtecatl.main.respositories;
 
-public interface PedidoRepository {
+
+import com.org.mixtecatl.main.models.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido,Long> {
 }
--- src/main/java/com/org/mixtecatl/main/services/PedidoService.java
+++ src/main/java/com/org/mixtecatl/main/services/PedidoService.java
@@ -1,4 +1,67 @@
 package com.org.mixtecatl.main.services;
 
+import com.org.mixtecatl.main.models.Pedido;
+import com.org.mixtecatl.main.respositories.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PedidoService {
+
+    private final PedidoRepository pedidoRepository;
+
+    @Autowired
+    public PedidoService(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+
+
+    public List<Pedido> getPedidos() {
+        return pedidoRepository.findAll();
+    }//getPedidos
+
+
+    public Optional<Pedido> getPedido(Long id) {
+        return pedidoRepository.findById(id);
+    }//getPedido
+
+
+    public Pedido postPedido(Pedido pedido) {
+        return pedidoRepository.save(pedido);
+    }//postPedido
+
+
+    public Pedido putPedido(Long id, Pedido pedidoActualizado) {
+
+        Optional<Pedido> pedidoE = pedidoRepository.findById(id);
+
+        if (pedidoE.isPresent()) {
+
+            Pedido pedido = pedidoE.get();
+
+            pedido.setFechaSolicitud(pedidoActualizado.getFechaSolicitud());
+            pedido.setEstadoPedido(pedidoActualizado.getEstadoPedido());
+            pedido.setPlatillo(pedidoActualizado.getPlatillo());
+            pedido.setUsuario(pedidoActualizado.getUsuario());
+
+            return pedidoRepository.save(pedido);
+        }
+
+        return null;
+    }//putPedido
+
+
+    public Pedido deletePedido(Long id) {
+        Pedido pedido = null;
+        if (pedidoRepository.existsById(id)) {
+            pedido = pedidoRepository.findById(id).get();
+            pedidoRepository.deleteById(id);
+        }
+
+        return pedido;
+    }//deletePedido
+
 }
