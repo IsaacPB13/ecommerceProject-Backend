bfa8153c721a289b02e6a463775e1e5d39351fab
MichaelC <michaelcastrorom30@gmail.com>
2026-07-14 11:05:55 -0600

    add changes

--- src/main/java/com/org/mixtecatl/main/services/PedidoService.java
+++ src/main/java/com/org/mixtecatl/main/services/PedidoService.java
@@ -44,7 +44,6 @@ public class PedidoService {
 
             pedido.setFechaSolicitud(pedidoActualizado.getFechaSolicitud());
             pedido.setEstadoPedido(pedidoActualizado.getEstadoPedido());
-            pedido.setPlatillo(pedidoActualizado.getPlatillo());
             pedido.setUsuario(pedidoActualizado.getUsuario());
 
             return pedidoRepository.save(pedido);
