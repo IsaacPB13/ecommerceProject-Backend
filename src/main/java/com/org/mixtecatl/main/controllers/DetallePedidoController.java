d6d1a05b6b9274de420a2560cd73c0cad21aa493
IsaacPB13 <iperezb1308@gmail.com>
2026-07-16 16:37:04 -0600

    Actualizando rutas

--- src/main/java/com/org/mixtecatl/main/controllers/DetallePedidoController.java
+++ src/main/java/com/org/mixtecatl/main/controllers/DetallePedidoController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/detallePedidos")
+@RequestMapping("/mixapi/detallePedidos")
 @CrossOrigin(origins = "*")
 public class DetallePedidoController {
 
